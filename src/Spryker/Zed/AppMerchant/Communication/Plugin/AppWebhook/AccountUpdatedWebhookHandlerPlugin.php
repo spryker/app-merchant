96ee27565268e96c48c3944784932381621b560c
René Klatt <rene.klatt@spryker.com>
2024-07-04 15:01:52 +0200

    CR Fixes

--- src/Spryker/Zed/AppMerchant/Communication/Plugin/AppWebhook/AccountUpdatedWebhookHandlerPlugin.php
+++ src/Spryker/Zed/AppMerchant/Communication/Plugin/AppWebhook/AccountUpdatedWebhookHandlerPlugin.php
@@ -13,10 +13,6 @@ use Spryker\Zed\AppWebhook\Dependency\Plugin\WebhookHandlerPluginInterface;
 use Spryker\Zed\Kernel\Communication\AbstractPlugin;
 
 /**
- * {@inheritDoc}
- *
- * @api
- *
  * @method \Spryker\Zed\AppMerchant\AppMerchantConfig getConfig()
  * @method \Spryker\Zed\AppMerchant\Business\AppMerchantFacadeInterface getFacade()
  */
