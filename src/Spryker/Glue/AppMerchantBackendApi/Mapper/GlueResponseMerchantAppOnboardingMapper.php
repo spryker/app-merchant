e21e4e07cc9b3dd6b75ba47d77d8eb94257a48fd
vol4onok <alexander.veselov@spryker.com>
2024-09-03 09:57:41 +0300

    ACP-3779: Adjusted mapper

--- src/Spryker/Glue/AppMerchantBackendApi/Mapper/GlueResponseMerchantAppOnboardingMapper.php
+++ src/Spryker/Glue/AppMerchantBackendApi/Mapper/GlueResponseMerchantAppOnboardingMapper.php
@@ -10,7 +10,6 @@ namespace Spryker\Glue\AppMerchantBackendApi\Mapper;
 use Generated\Shared\Transfer\GlueErrorTransfer;
 use Generated\Shared\Transfer\GlueResponseTransfer;
 use Generated\Shared\Transfer\MerchantAppOnboardingResponseTransfer;
-use Generated\Shared\Transfer\MerchantTransfer;
 use Symfony\Component\HttpFoundation\Response;
 
 class GlueResponseMerchantAppOnboardingMapper implements GlueResponseMerchantAppOnboardingMapperInterface
@@ -27,8 +26,7 @@ class GlueResponseMerchantAppOnboardingMapper implements GlueResponseMerchantApp
         MerchantAppOnboardingResponseTransfer $merchantAppOnboardingResponseTransfer,
         GlueResponseTransfer $glueResponseTransfer
     ): GlueResponseTransfer {
-        /** @phpstan-var \Generated\Shared\Transfer\MerchantTransfer */
-        $merchantTransfer = $merchantAppOnboardingResponseTransfer->getMerchantOrFail();
+        $merchantTransfer = $merchantAppOnboardingResponseTransfer->getMerchant();
         $merchantAppOnboarding = $merchantTransfer !== null  ? $merchantTransfer->toArray() : [];
         $merchantAppOnboarding[MerchantAppOnboardingResponseTransfer::STRATEGY] = $merchantAppOnboardingResponseTransfer->getStrategy();
         $merchantAppOnboarding[MerchantAppOnboardingResponseTransfer::URL] = $merchantAppOnboardingResponseTransfer->getUrl();
